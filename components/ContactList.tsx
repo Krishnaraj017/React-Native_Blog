19ddb1116823af8469ca71da85d13e9188a73fe9
Krishnaraj S <krishnarajshetty699@gmail.com>
2023-05-11 22:57:04 +0530

    some commit

--- components/ContactList.tsx
+++ components/ContactList.tsx
@@ -41,8 +41,10 @@ export default function ContactList() {
               }}
               style={styles.userImage}
             />
-            <Text style={styles.userName}>{name}</Text>
-            <Text style={styles.userStatus}>{status}</Text>
+            <View>
+              <Text style={styles.userName}>{name}</Text>
+              <Text style={styles.userStatus}>{status}</Text>
+            </View>
             <Text>{}</Text>
           </View>
         ))}
@@ -52,15 +54,54 @@ export default function ContactList() {
 }
 
 const styles = StyleSheet.create({
-  headingText: {},
-  container: {},
-  userCard:{},
+  headingText: {
+    fontSize:24,
+    fontWeight:'bold',
+    paddingHorizontal:8,
+    paddingVertical:2
+
+
+
+  },
+  container: {
+  
+    flex:1,
+    width:330,
+    height:'auto',
+    flexDirection:'column',
+    marginHorizontal:16,
+    marginVertical:12,
+    borderRadius:8,
+    
+
+  },
+  userCard:{
+    flex:1,
+    flexDirection:'row',
+   // alignItems:'center',
+    marginBottom:3,
+    backgroundColor:'#8D3DAF',
+    padding:8,
+    borderRadius:12,
+
+
+  },
   userImage:{
     width:60,
     height:60,
-    borderRadius:60/2
+    borderRadius:60/2,
+    marginRight:14,
   },
-  userName:{},
-  userStatus:{},
+  userName:{
+    fontSize:16,
+    fontSWeight:'600',
+    color:'#ffff'
+
+
+  },
+  userStatus:{
+    fontSize:16,
+    
+  },
 
 });
